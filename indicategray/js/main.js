2b9cf35f8b4f2e9454b85dca3b8d88bd03455993
Evan Lawrence-Hurt <evanlh@gmail.com>
2014-02-26 21:37:08 -0500

    more inidciate gray

--- indicategray/js/main.js
+++ indicategray/js/main.js
@@ -3,29 +3,22 @@
     var pageload = function() {
         var w, h;
         if ($('.process').length) {
-            w = h = 700;
+            w = h = 600;
         } else {
             w = h = 935;
         }
+        console.log(w);
         $('#slides').slidesjs({
             width: w,
             height: h,
             callback: {
                 loaded: function(number) {
-                    // Use your browser console to view log
-                    console.log('SlidesJS: Loaded with slide #' + number);
-
                     // Show start slide in log
                     $('#slidesjs-log .slidesjs-slide-number').text(number);
                 },
                 start: function(number) {
-                    // Use your browser console to view log
-                    console.log('SlidesJS: Start Animation on slide #' + number);
                 },
                 complete: function(number) {
-                    // Use your browser console to view log
-                    console.log('SlidesJS: Animation Complete. Current slide is #' + number);
-
                     // Change slide number on animation complete
                     $('#slidesjs-log .slidesjs-slide-number').text(number);
                 }
@@ -45,8 +38,8 @@
                 $content.load(href + " .content >*", function(responseText) {
                     pagecache[href] = responseText;
                     History.pushState(null, null, href);
-                    pageload();
                     $content.animate({'opacity': 1});
+                    pageload();
                 });
             // }
         });
