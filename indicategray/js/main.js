bb7a34fb402768a6b5898a578f65395615715703
Evan Lawrence-Hurt <evanlh@gmail.com>
2014-02-26 20:31:53 -0500

    new indicate gray

--- indicategray/js/main.js
+++ indicategray/js/main.js
@@ -1,39 +1,66 @@
-var pageload = function() {
-    $('#slides').slidesjs({
-        //    width: 940,
-        //   height: 528,
-        callback: {
-            loaded: function(number) {
-                // Use your browser console to view log
-                console.log('SlidesJS: Loaded with slide #' + number);
-
-                // Show start slide in log
-                $('#slidesjs-log .slidesjs-slide-number').text(number);
-            },
-            start: function(number) {
-                // Use your browser console to view log
-                console.log('SlidesJS: Start Animation on slide #' + number);
-            },
-            complete: function(number) {
-                // Use your browser console to view log
-                console.log('SlidesJS: Animation Complete. Current slide is #' + number);
-
-                // Change slide number on animation complete
-                $('#slidesjs-log .slidesjs-slide-number').text(number);
-            }
+(function() {
+    var pagecache = {};
+    var pageload = function() {
+        var w, h;
+        if ($('.process').length) {
+            w = h = 700;
+        } else {
+            w = h = 935;
         }
-    });
-};
+        $('#slides').slidesjs({
+            width: w,
+            height: h,
+            callback: {
+                loaded: function(number) {
+                    // Use your browser console to view log
+                    console.log('SlidesJS: Loaded with slide #' + number);
 
-$('.navbar a').click(function() {
-    var $this = $(this),
-        $content = $('.content');
-    $content.animate({'opacity': 0}, function() {
-        $('.content').load($this.attr('href'), function() {
-            pageload();
-            $content.animate({'opacity': 1});
+                    // Show start slide in log
+                    $('#slidesjs-log .slidesjs-slide-number').text(number);
+                },
+                start: function(number) {
+                    // Use your browser console to view log
+                    console.log('SlidesJS: Start Animation on slide #' + number);
+                },
+                complete: function(number) {
+                    // Use your browser console to view log
+                    console.log('SlidesJS: Animation Complete. Current slide is #' + number);
+
+                    // Change slide number on animation complete
+                    $('#slidesjs-log .slidesjs-slide-number').text(number);
+                }
+            }
+        });
+    };
+
+    $(window).bind('statechange', function() {
+        var $content = $('.content'),
+            State = History.getState(),
+            href = State.url;
+
+        $content.animate({'opacity': 0}, function() {
+            // if (typeof pagecache[State.hash] != 'undefined') {
+            //     $('.content').empty().append(pagecache[State.hash]);
+            // } else {
+                $content.load(href + " .content >*", function(responseText) {
+                    pagecache[href] = responseText;
+                    History.pushState(null, null, href);
+                    pageload();
+                    $content.animate({'opacity': 1});
+                });
+            // }
         });
     });
-    return false;
-});
 
+    $('.navbar a').click(function(event) {
+        var $this = $(this),
+            href = $this.attr('href');
+        if (event.which == 2 || event.metaKey) { return true; }
+        History.pushState(null, null, href);
+        return false;
+    });
+
+
+    pageload();
+
+})();
